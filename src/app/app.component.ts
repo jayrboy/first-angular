9898e032ec7f6163436a0380cd8d76b0053e12b8
jayrboy <j.jayox_@hotmail.com>
2024-05-18 14:16:11 +0700

    home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  // templateUrl: './app.component.html',
-  template: ` <h1>Hello World!</h1> `,
-  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'homes';
