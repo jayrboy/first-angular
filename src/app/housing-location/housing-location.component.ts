bbd0c1576d99f8a37ebe566976474ff1d5780adb
jayrboy <j.jayox_@hotmail.com>
2024-05-18 14:46:55 +0700

    HousingLocation component

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './housing-location.component.html',
+})
+export class HousingLocationComponent {
+
+}
