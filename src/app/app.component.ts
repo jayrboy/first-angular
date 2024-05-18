5c9e4a89da470716a20a3c98b0e3a2aaa25ee63b
jayrboy <j.jayox_@hotmail.com>
2024-05-18 13:08:34 +0700

    hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  template: ` <h1>Hello World!</h1> `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'first-angular';
+  title = 'homes';
 }
