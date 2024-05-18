3f408bccf2d1bff46d0f9479a035b353ce6517e6
jayrboy <j.jayox_@hotmail.com>
2024-05-18 14:53:04 +0700

    add an input parameter

--- src/app/views/housing-location/housing-location.component.ts
+++ src/app/views/housing-location/housing-location.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../../models/housinglocation.model';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
